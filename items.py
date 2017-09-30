32b2a45e1ac39b37c1ff7cdd97a53e1565d0d1d4
Rico Ullmann <rico@erinnerungsfragmente.de>
2017-09-30 19:01:31 +0200

    update to bw 3.0

--- items.py
+++ items.py
@@ -5,7 +5,6 @@ pkg_dnf = {
 
 svc_systemd = {
     'php-fpm': {
-        'enabled': True,
         'needs': [
             'pkg_dnf:php-fpm',
         ],
@@ -15,8 +14,6 @@ svc_systemd = {
 files = {
     '/etc/php-fpm.conf': {
         'source': "php-fpm.conf",
-        'owner': "root",
-        'group': "root",
         'mode': "0644",
         'content_type': "mako",
         'needs': [
@@ -28,8 +25,6 @@ files = {
     },
     '/etc/php.ini': {
         'source': 'php.ini',
-        'owner': 'root',
-        'group': 'root',
         'mode': '0644',
         'content_type': 'mako',
         'needs': [
@@ -55,8 +50,6 @@ for pkg in node.metadata['php']['packages']:
 for pool_name, pool_options in sorted(node.metadata.get('php', {}).get('fpm_pools', {}).items()):
     files['/etc/php-fpm.d/{}.conf'.format(pool_name)] = {
         'source': 'fpm_pool_template',
-        'owner': 'root',
-        'group': 'root',
         'mode': '0644',
         'content_type': 'mako',
         'context': {
@@ -75,8 +68,6 @@ if node.has_bundle("monit"):
     files['/etc/monit.d/php-fpm'] = {
         'source': "monit",
         'mode': "0640",
-        'owner': "root",
-        'group': "root",
         'content_type': "mako",
         'triggers': [
             "svc_systemd:monit:restart",
